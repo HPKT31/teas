b87559ce6bb45d70036886cc6793737faad5b7fb
hoangphat <phatdhps31981@fpt.edu.vn>
2024-03-03 09:50:26 +0700

    ok

--- app/config/configs.php
+++ app/config/configs.php
@@ -1,5 +1,5 @@
 <?php
-define("APP_URL","http://localhost/Xuong/");
+define("APP_URL","http://localhost/TEAS/");
 define("APP_URLadmin","http://localhost/Xuong/Admin/");
 define('DB_HOST', 'localhost');
 define('DBUSER','root');
